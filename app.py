248d23cd268da2971183970d161d137d803f30d4
Timoy <timotyjoel2811@gmail.com>
2025-02-09 17:50:53 +0700

    feat: Complete the Initial Question + Need to improve to be interactive

--- app.py
+++ app.py
@@ -39,10 +39,10 @@ selected_year = st.sidebar.slider(
 
 # Apply the filter
 genres = df['Genre'].unique()
-selected_genre = st.sidebar.multiselect("Select the Genre", genres)
+selected_genre = st.sidebar.multiselect("Select the Genre", genres, default = df['Genre'].unique())
 
 platform = df['Platform'].unique()
-selected_platform = st.sidebar.multiselect("Select the Platform", platform)
+selected_platform = st.sidebar.multiselect("Select the Platform", platform, default = df['Platform'].unique())
 
 filtered_df = df[
     (df['Platform'].isin(selected_platform)) & 
@@ -58,15 +58,52 @@ st.header("Overview Metrics")
 st.metric("Top Game ", f"{total_sales:.2f}")
 st.metric("Top Game by Sales", top_game)
 
+col1, col2 = st.columns(2)
+
 # Visualization
-st.subheader("Total Sales by Platform")
+with col1:
+    st.subheader("Total Sales by Platform")
+    if not filtered_df.empty:
+        platform_sales = filtered_df.groupby('Platform')['Total_Global_Sales'].sum().sort_values(ascending=False)
+        fig, ax = plt.subplots(figsize = (8, 4))
+        platform_sales.plot(kind = 'bar', ax = ax, color = 'skyblue')
+        ax.set_ylabel('Sales')
+        ax.set_xlabel('Platform')
+        ax.set_title('Total Sales by Platform')
+        st.pyplot(fig)
+    else:
+        st.warning("No Data Available to be Performed")
+
+with col2:
+    st.subheader("Total Sales by Genre")
+    if not filtered_df.empty:
+        genre_sales = filtered_df.groupby('Genre')['Total_Global_Sales'].sum().sort_values(ascending=False)
+        fig, ax = plt.subplots(figsize=(8, 4))
+        genre_sales.plot(kind = 'bar', ax = ax, color = 'orange')
+        ax.set_ylabel('Sales')
+        ax.set_xlabel('Genre')
+        ax.set_title('Total Sales based on Genre')
+        st.pyplot(fig)
+    else:
+        st.warning('No Data Available to be Performed')
+    
+# Year Sales
+st.subheader("Yearly Sales Trend")
 if not filtered_df.empty:
-    platform_sales = filtered_df.groupby('Platform')['Total_Global_Sales'].sum().sort_values(ascending=False)
+    yearly_sales = filtered_df.groupby('Year_of_Release')['Total_Global_Sales'].sum()
     fig, ax = plt.subplots(figsize = (8, 4))
-    platform_sales.plot(kind='bar', ax=ax, color = 'skyblue')
-    ax.set_ylabel('Sales')
-    ax.set_xlabel('Platform')
-    ax.set_title('Total Sales by Platform')
-    st.pyplot(fig)
+    yearly_sales.plot(kind = 'line', ax = ax, color = 'red')
+    ax.set_ylabel('Yearly Sales')
+    ax.set_xlabel('Year')
+    ax.set_title('Sales Trend')
+    st.pyplot()
 else:
-    st.warning("No Data Available to be Performed")
+    st.warning('No Data Available to be Performed')
+
+# Top 10 Games
+st.subheader('Top 10 Games by Sales')
+if not filtered_df.empty:
+    top_games = filtered_df[['Name', 'Total_Global_Sales']].sort_values(by='Total_Global_Sales', ascending=False).head(10)
+    st.table(top_games)
+else:
+    st.warning("No data available for the selected filters.")
